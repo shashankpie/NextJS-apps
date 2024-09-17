82135595a22bfcadb00c177c7b645b2232df8a80
Shiva Shashank <shashanktech99@gmail.com>
2024-09-17 18:27:45 -0500

    updates

--- Stack-Overflow/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Hello world from Next.js 14</h1>
-      <h2 className="h2-bold">Hello world from Next.js 14</h2>
-      <h3 className="h3-bold">Hello world from Next.js 14</h3>
-    </div>
-  );
-}
